577c2c16a89db453849077373be616445e73a15e
Yura Avramenko <jaerbi42@gmail.com>
2019-07-13 13:32:28 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+import {AngularFireModule} from 'angularfire2';
+import {AngularFirestoreModule} from 'angularfire2/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAWojNnC8eyWBQhjmMBg9_abB5XBqKQZ5s',
+  authDomain: 'llll-d845d.firebaseapp.com',
+  databaseURL: 'https://llll-d845d.firebaseio.com/',
+  projectId: 'llll-d845d',
+  storageBucket: '',
+  messagingSenderId: '189381062081'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
